efb71a3ce23838ebe46485eafe73b6b04376d560
YinChengChen <xenia0213@hotmail.com>
2023-06-29 11:18:11 +0800

    version 2.0.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 import MyNav from "./pages/MyNav";
-import Home from "./pages/Home";
-const testEl = ReactDOM.createRoot(
-  document.getElementById("here") as HTMLDivElement
-)
+// import Home from "./pages/Home";
+// const testEl = ReactDOM.createRoot(
+//   document.getElementById("here") as HTMLDivElement
+// )
+// const testEl = ReactDOM.createRoot(
+//   
+// );
 // testEl.render(
 //   <MyNav/>
 // )
 
-const homeEl = ReactDOM.createRoot(
-  document.getElementById("home-block") as HTMLDivElement
-)
+// const homeEl = ReactDOM.createRoot(
+//   document.getElementById("home-block") as HTMLDivElement
+// );
 // homeEl.render(
-  // <Home/>
+// <Home/>
 // )
