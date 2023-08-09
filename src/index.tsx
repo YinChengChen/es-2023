6c9c11c1d68edf29229f71f8b0d52fb1c5ef5bab
YinChengChen <xenia0213@hotmail.com>
2023-08-09 11:42:37 +0800

    version 2.7.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
-import MyNav from "./pages/MyNav";
+// import MyNav from "./pages/MyNav";
 // import Home from "./pages/Home";
 // const testEl = ReactDOM.createRoot(
 //   document.getElementById("here") as HTMLDivElement
